a09e8b65ececa5b521b90ec6d20176032620f389
Garbi-Collector <uwurulos@gmail.com>
2026-02-13 00:16:41 -0300

    componente diceroll

--- src/app/pages/dice-roller/dice-roller.component.ts
+++ src/app/pages/dice-roller/dice-roller.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dice-roller',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dice-roller.component.html',
   styleUrl: './dice-roller.component.scss'
 })
 export class DiceRollerComponent {
-
+  @Input() isRolling = false;
+  @Input() showResult = false;
+  @Input() diceResults: number[] = [];
+  @Input() droppedIndex = -1;
+  @Input() total = 0;
 }
